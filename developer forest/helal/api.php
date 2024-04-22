c13103bb68963ec15cda233572a528bde3f0828a
alnazad <nazadahammed850@gmail.com>
2024-04-23 00:25:39 +0600

    Delete developer forest directory

--- developer forest/helal/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id = $_GET['id'];
-global $wpdb;
-$r_table = $wpdb->prefix . "raw_materials";
-$data=$wpdb->get_row("select * from $r_table where id=$id");
-//header('Content-Type: application/json');
-echo json_encode($data);
--- developer forest/rakib/buyers_delete.php
+++
@@ -1,5 +0,0 @@
-<?php
-global $wpdb;
-$table = $wpdb->prefix . "secondary_buyers";
-$data = $wpdb->get_row("select * from $table where id=" . $_GET['id']);
-
